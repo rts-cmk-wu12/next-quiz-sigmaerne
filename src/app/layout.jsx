e3cbbfe60ad473d34b671cd5e4db705f4faf1ab7
GR0UD <marksgalkins12345678910@gmail.com>
2025-06-25 10:33:49 +0200

    First Setup.

---
+++ src/app/layout.jsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "QuizMe",
+  description: `Try me!
+  Best Quiz App made by Mark & Peter`,
+  icons: {
+    icon: "/images/favicon.png",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body className='font-[--font-main] antialiased bg-[--bg] text-[--text]'>
+        {children}
+      </body>
+    </html>
+  );
+}
