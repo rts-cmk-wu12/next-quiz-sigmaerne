cfee2a4c53f67ad454d9672ff66ee03c476b953b
Johndillerman <U2486892@rts-365.dk>
2025-06-25 10:50:15 +0200

    qu!zme

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,9 @@
 
 export default function Home() {
   return (
-    <main>
-      <section>
-        <h1>quizme!</h1>
+    <main className="flex items-center justify-center min-h-screen">
+      <section className="flex justify-center">
+        <h1 className=" text-6xl " >qu!zme</h1>
       </section>
     </main>
   );
