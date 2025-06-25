094c850e6a32abb1d28883be48fca0d00ee43136
Johndillerman <U2486892@rts-365.dk>
2025-06-25 10:41:34 +0200

    page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <main>
-      <div></div>
+      <section>
+        <h1>quizme!</h1>
+      </section>
     </main>
   );
 }
